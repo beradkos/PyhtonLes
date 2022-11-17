65dce160a101cbcd43494f87caa70413027e9f9b
beradkos <115186101+beradkos@users.noreply.github.com>
2022-11-17 20:37:19 +0100

    1

--- Matrix.py
+++ Matrix.py
@@ -1,1 +1,10 @@
-matrix =
+punten = []
+hulplijst = []
+for i in range(0,2):#
+    for j in range(0,3):#
+        getal = int(input("geef je getal"))
+        hulplijst.append(getal)
+        punten.append(hulplijst)
+        hulplijst = []
+som = 0
+
---
+++ def percentage.py
@@ -0,0 +1,5 @@
+def percentage(getal,procent):
+    return getal/100*procent
+
+btw = percentage(200,21)
+print(btw)
